f21e16b0506ab28bad83a6ed5a256ce3513c6287
SarunasAzna <SarunasAzna@users.noreply.github.com>
2017-07-04 14:14:36 +0200

    remove old url definition

--- loginurl/urls.py
+++ loginurl/urls.py
@@ -1,14 +1,14 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import RedirectView
 from django.conf import settings
 
 from loginurl.views import cleanup, login
 
-urlpatterns = patterns('',
-    (r'^cleanup/$', cleanup),
-    (r'^(?P<key>[0-9A-Za-z]+-[a-z0-9-]+)/$', login), 
+urlpatterns = [
+    url(r'^cleanup/$', cleanup),
+    url(r'^(?P<key>[0-9A-Za-z]+-[a-z0-9-]+)/$', login), 
     url(r'^$', name='loginurl-index', view=RedirectView.as_view(
         permanent=True,
         url=settings.LOGIN_URL,
     )),
-)
+]
