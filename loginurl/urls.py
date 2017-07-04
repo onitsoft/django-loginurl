258fef8b725da257438ce417a3a354eac454a537
SarunasAzna <SarunasAzna@users.noreply.github.com>
2017-07-04 15:07:15 +0200

    names to urls

--- loginurl/urls.py
+++ loginurl/urls.py
@@ -5,10 +5,12 @@ from django.conf import settings
 from loginurl.views import cleanup, login
 
 urlpatterns = [
-    url(r'^cleanup/$', cleanup),
-    url(r'^(?P<key>[0-9A-Za-z]+-[a-z0-9-]+)/$', login), 
-    url(r'^$', name='loginurl-index', view=RedirectView.as_view(
+    url(r'^cleanup/$', cleanup, name='loginurl-cleanup'),
+    url(r'^(?P<key>[0-9A-Za-z]+-[a-z0-9-]+)/$', login,
+        name='loginurl-login'), 
+    url(r'^$', RedirectView.as_view(
         permanent=True,
-        url=settings.LOGIN_URL,
-    )),
+        url=settings.LOGIN_URL)
+        name='loginurl-index'
+       ),
 ]
